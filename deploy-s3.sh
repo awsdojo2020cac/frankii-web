39326077f5aa02691ab2254911f231c5e7b9693b
JonathanLee13 <498ef85f264a2668d324b772d6aa800f45cfc0ac>
2020-07-20 09:07:49 +0900

    init

--- deploy-s3.sh
+++ deploy-s3.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+
 aws s3 rm s3://frankii-website/ --recursive
 aws s3 cp dist s3://frankii-website/ --recursive
--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,55 @@
 <template>
 
-  <div id="app">
-  <v-app>
-      <h1>Frankii</h1>
-    <v-container fluid width="700">
-    質問文生成
+    <div id="app">
+        <v-app>
+            <h1>Frankii</h1>
+            <v-container fluid width="700">
+                質問文生成
 
-    <v-row align="center">
-    <v-col cols=2>
-    <v-select
-     class="my-2"
-          :items = "categories"
-             label="カテゴリ"
-                                          filled dense
-                                        ></v-select>
-                                           <v-col cols=2>
-                                            <v-text-field v-model="category"></v-text-field>
-                                            </v-col>
-                                        </v-col>
-                                        </v-row>
-                                        </v-container>
-                                        <v-btn @click=submitToAPI>質問文生成</v-btn>
-                                        <v-card>Hi</v-card>
-</v-app>
+                <v-row align="center">
+                    <v-col cols=2>
+                        <v-select
+                                class="my-2"
+                                :items="categories"
+                                label="カテゴリ"
+                                filled dense
+                        ></v-select>
+                        <v-col cols=2>
+                            <v-text-field v-model="category"></v-text-field>
+                        </v-col>
+                    </v-col>
+                </v-row>
+            </v-container>
+            <v-btn @click=submitToAPI>質問文生成</v-btn>
+            <v-card>Hi</v-card>
+        </v-app>
 
-  </div>
+    </div>
 </template>
 
 <script>
 
-export default {
-  name: 'App',
-  data:() => ({
-        category: "エラ",
-        categories:['エラー','ネットワークエラ-']
-     }),
-   methods:{
-        submitToAPI(){
-            console.log("api")
+    export default {
+        name: 'App',
+        data: () => ({
+            category: "エラ",
+            categories: ['エラー', 'ネットワークエラ-']
+        }),
+        methods: {
+            submitToAPI() {
+                console.log("api")
+            }
         }
-   }
-}
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 30px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 30px;
+    }
 </style>
